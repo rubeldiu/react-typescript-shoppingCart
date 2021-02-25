41a58dc0abf219130eb8e9f70dca0d30d22453d8
hamiduldiu <hamiduldiu@gmail.com>
2021-02-25 11:10:23 +0100

    ShoppingCart with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient,QueryClientProvider} from 'react-query';
+const client =new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+     <QueryClientProvider client={client}>
+       <App />
+     </QueryClientProvider>
+    
+  ,
   document.getElementById('root')
 );
 
